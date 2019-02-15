20109dac118b34f55b152f4d82aca444f4cee1aa
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-15 19:57:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,8 @@
 # Write your code here.
 def line(array)
   result = "The line is currently: "
-  
+  for i in 0...array.length
+    result += "#{i + 1}. #{array[i]}#{i == array.length-1 ? "" : ", "}"
+  end
+  puts result
 end
