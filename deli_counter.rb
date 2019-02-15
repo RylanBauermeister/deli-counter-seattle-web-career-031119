ae5e9e53bd8fa801e76dbc1ac3783f01a3ddcf16
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-15 19:58:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,8 @@
 # Write your code here.
 def line(array)
-  if array.empty
+  if array.empty?
+    return "The line is currently empty."
+  end
   result = "The line is currently: "
   for i in 0...array.length
     result += "#{i + 1}. #{array[i]}#{i == array.length-1 ? "" : ", "}"
